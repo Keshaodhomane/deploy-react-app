7ee03d8f22ee99cf3572544c01a04818e58009c1
Keshao Dhomane <keshaodhomane@gmail.com>
2019-08-30 08:26:01 +0530

    files

---
+++ precache-manifest.dfe834d4594fb5f58a6ee61ffd444a9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4566540597a091ada4e5b2e27083b77a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f433c3ac87ccf32d601",
+    "url": "/static/css/main.beceffbe.chunk.css"
+  },
+  {
+    "revision": "a059e1e1ded54577d96f",
+    "url": "/static/js/2.4d5a7f80.chunk.js"
+  },
+  {
+    "revision": "9f433c3ac87ccf32d601",
+    "url": "/static/js/main.92a0bac1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
